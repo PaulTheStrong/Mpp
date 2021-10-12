3db1c8acef41db3410faa10ca19234cb99e1207e
unknown <pavel_saroka@epam.com>
2021-10-12 23:52:40 +0300

    plugin generators

---
+++ DateGenerator/DateGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Generator
+{
+    public class DateGenerator : IGenerator
+    {
+        public object generateValue()
+        {
+            var random = new Random();
+            return new DateTime(
+                random.Next(25) + 2000, random.Next(12),
+                random.Next(29), random.Next(12), 
+                random.Next(60), random.Next(60));
+        }
+
+        public Type getGeneratedType()
+        {
+            return typeof(DateTime);
+        }
+    }
+}
---
+++ StringGenerator/StringGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace Generator
+{
+    public class StringGenerator : IGenerator
+    {
+        public object generateValue()
+        {
+            var random = new Random();
+            int length = random.Next(20) + 10;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                sb.Append((char) ('0' + random.Next(60)));
+            }
+            return sb.ToString();
+        }
+        public Type getGeneratedType()
+        {
+            return typeof(string);
+        }
+    }
+}
